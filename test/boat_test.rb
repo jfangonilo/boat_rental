6ec5947045c3ce97268973f69900af13098ebc89
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-17 11:34:51 -0600

    Complete iteration 1

---
+++ test/boat_test.rb
@@ -0,0 +1,31 @@
+require './lib/boat'
+require 'minitest/pride'
+require 'minitest/autorun'
+
+class BoatTest < Minitest::Test
+ def setup
+   @kayak = Boat.new(:kayak, 20)
+ end
+
+ def test_it_exists
+   assert_instance_of Boat, @kayak
+ end
+
+ def test_it_has_attributes
+   assert_equal :kayak, @kayak.type
+   assert_equal 20, @kayak.price_per_hour
+ end
+
+ def test_it_starts_with_zero_hours_rented
+   assert_equal 0, @kayak.hours_rented
+ end
+
+ def test_you_can_add_hours
+  @kayak.add_hour
+  assert_equal 1, @kayak.hours_rented
+  @kayak.add_hour
+  assert_equal 2, @kayak.hours_rented
+  @kayak.add_hour
+  assert_equal 3, @kayak.hours_rented
+ end
+end
